ea3adfaf0b053c3ede0c183835a6af2e8cad250b
PramudyaReynaldi <prmdy02@gmail.com>
2024-02-25 05:42:04 +0700

    testingg

--- middleware/RequireAuth.js
+++ middleware/RequireAuth.js
@@ -1,18 +1,11 @@
-// export const requireAuth = (req, res, next) => {
-//     try {
-//         if (!req.session.userId) {
-//             return res.status(401).json({ error: "Unauthorized", msg: "Mohon Login Terlebih Dahulu" });
-//         }
-//         next();
-//     } catch (error) {
-//         console.error("Error in requireAuth middleware:", error);
-//         res.status(500).json({ error: "Internal Server Error", msg: "Terjadi kesalahan internal" });
-//     }
-// };
-
 export const requireAuth = (req, res, next) => {
-    if (!req.session.userId) {
-        return res.status(401).json({ error: "Unauthorized", msg: "Mohon Login Terlebih Dahulu" });
+    try {
+        if (!req.session.userId) {
+            return res.status(401).json({ error: "Unauthorized", msg: "Mohon Login Terlebih Dahulu" });
+        }
+        next();
+    } catch (error) {
+        console.error("Error in requireAuth middleware:", error);
+        res.status(500).json({ error: "Internal Server Error", msg: "Terjadi kesalahan internal" });
     }
-    next();
 };
