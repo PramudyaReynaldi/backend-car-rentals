70eaff2f968648235f94fdb3f62e1e7186de473e
PramudyaReynaldi <prmdy02@gmail.com>
2024-02-25 05:23:00 +0700

    test login

--- routes/AuthRoute.js
+++ routes/AuthRoute.js
@@ -4,7 +4,8 @@ import { requireAuth } from "../middleware/RequireAuth.js";
 
 const router = express.Router();
 
-router.get("/api/v1/me", requireAuth, Me);
+// router.get("/api/v1/me", requireAuth, Me);
+router.get("/api/v1/me", Me);
 router.post("/api/v1/login", Login);
 router.delete("/api/v1/logout", logOut);
 router.post("/api/v1/register", Register);
